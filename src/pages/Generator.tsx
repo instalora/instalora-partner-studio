24e8c8ea6428d23b36e3c40ada5520e9daccaf90
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2025-12-15 20:48:22 +0200

    Update Generator.tsx

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -599,7 +599,7 @@ const Generator = () => {
 
                 {generationItems.length ? (
                   <div className="space-y-4">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <div className="grid">
                       {generationItems.map((item, index) => {
                         const promptPreview = truncateText(item.prompt);
                         const assets = normalizeAssets(item);
